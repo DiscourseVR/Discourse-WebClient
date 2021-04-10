1f8e311b88b7f0f2b1b08656ba06417fa14da430
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-04-10 13:43:06 -0700

    vid working

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { getVideoIndex } from '../services/videos.service'
 
 export default function Home() {
-
   return (
     <div>
-    <h1>DiscourseVR</h1>
+      <h1>DiscourseVR</h1>
 
-    <button onClick={makeRequest}>get!</button>
+      <button onClick={makeRequest}>get!</button>
+
+      <video src='https://files.discoursevr.space/vids/2021-04-10-09-24-16.mp4' autoPlay muted>
+      </video>
     </div>
   )
 }
